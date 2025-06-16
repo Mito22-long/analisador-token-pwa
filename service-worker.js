02058f395011f2a3eb09b34ce45d30d5e7f72a7b
Mito22-long <Patriacoin22@gmail.com>
2025-06-15 22:57:19 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,44 @@
+const CACHE_NAME = 'analyzer-token-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './token-score.js',
+  './pwa.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './ptc-banner.png'
+];
 
-const CACHE_NAME = "analisador-token-cache-v1";
-const urlsToCache = ["index.html", "style.css", "token-score.js"];
-
-self.addEventListener("install", event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('✅ Cache aberto');
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('🧹 Deletando cache antigo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
